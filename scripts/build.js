1e1157103e8f64af7aac25150606bef3a0718b97
valleedgar085-rgb <valleedgar085@gmail.com>
2026-09-14 02:45:03 -0500

    Keep Create-to-Shape bridge inside existing app bundle budget

--- scripts/build.js
+++ scripts/build.js
@@ -72,22 +72,18 @@ fs.writeFileSync(
 );
 
 const indexSource = fs.readFileSync(path.join(projectRoot, 'index.html'), 'utf8');
-const builtIndex = indexSource
-  .replace(
+fs.writeFileSync(
+  path.join(wwwDir, 'index.html'),
+  indexSource.replace(
     '</head>',
     `<style>${creatorStyles.code}</style><style>${createWorkflowStyles.code}</style><link rel="stylesheet" href="./generation-experience.css"><link rel="stylesheet" href="./shape-director.css"></head>`,
-  )
-  .replace(
-    '</body>',
-    '<script type="module" src="./src/create-shape-bridge.js?v=20260914-1"></script></body>',
-  );
-fs.writeFileSync(path.join(wwwDir, 'index.html'), builtIndex);
+  ),
+);
 
 await build({
   entryPoints: [
     path.join(projectRoot, 'src', 'app.js'),
     path.join(projectRoot, 'src', 'generation-worker.js'),
-    path.join(projectRoot, 'src', 'ui', 'create-shape-bridge.js'),
   ],
   outdir: path.join(wwwDir, 'src'),
   bundle: true,
